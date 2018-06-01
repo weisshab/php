8ecd38b95686bc7d20bca017c08f4651dab1f739
weisshaarb <weisshaarb@10.134.130.155>
2018-06-01 11:57:07 +0200

    a

--- Validate Form/index.php
+++ Validate Form/index.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 
 <html>
-<body>
-	<?php 
-		$error = "";
-		if($_SERVER["REQUEST_METHOD"] == "POST") {
-			if(empty(htmlspecialchars(stripslashes(trim($_POST["name"]))))){
-				$error = "Kein Name";
-			}elseif(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
-				$error = "Ungültige E-Mail";
-			}else{
-				echo "Hi ". htmlspecialchars(stripslashes(trim($_POST["name"]))).
-				"<br> Die E-Mail lautet ". htmlspecialchars(stripslashes(trim($_POST["email"])));
-			}
-		}
-	 ?>
-
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-	Name: <input type="text" name="name"> <br>
-	E-Mail: <input type="text" name="email"> <br>
-	<input type="submit">
-	<?php echo $error; ?>
-	</form>
-</body>
+    <body>
+    	<?php 
+    		$error = "";
+    		if($_SERVER["REQUEST_METHOD"] == "POST") {
+    			if(empty(htmlspecialchars(stripslashes(trim($_POST["name"]))))){
+    				$error = "Kein Name";
+    			}elseif(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+    				$error = "Ungültige E-Mail";
+    			}else{
+    				echo "Hi ". htmlspecialchars(stripslashes(trim($_POST["name"]))).
+    				"<br> Die E-Mail lautet ". htmlspecialchars(stripslashes(trim($_POST["email"])));
+    			}
+    		}
+    	 ?>
+    
+    	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    	Name: <input type="text" name="name"> <br>
+    	E-Mail: <input type="text" name="email"> <br>
+    	<input type="submit">
+    	<?php echo $error; ?>
+    	</form>
+    </body>
 </html>
